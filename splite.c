df0ce309fbfc4f5991f78e0b586b0e592776bf4f
Huii <452246084@qq.com>
2015-05-12 19:58:28 +0800

    Update splite.c

--- splite.c
+++ splite.c
@@ -1,61 +1,61 @@
 # CNote
-
-void splite(int points[], int start, int end, int ran)
+# 随机切割数组：扑克中的随机切牌算法（10个元素的循环次数在5-9次之间）
+void splite(int points[], int start, int end, int rand)
 {
     int len = end-start+1;
     
     if(len%2==0){
         //偶数个元素
-        if(ran==(len/2-1+start)){
+        if(rand==(len/2-1+start)){
             
-            for(int i=start;i<=ran;i++){
-                points[i] ^= points[(end+1)-(ran-i+1)];
-                points[(end+1)-(ran-i+1)] ^= points[i];
-                points[i] ^= points[(end+1)-(ran-i+1)];
+            for(int i=start;i<=rand;i++){
+                points[i] ^= points[(end+1)-(rand-i+1)];
+                points[(end+1)-(rand-i+1)] ^= points[i];
+                points[i] ^= points[(end+1)-(rand-i+1)];
             }
             
-        }else if(ran<(len/2-1+start)){
+        }else if(rand<(len/2-1+start)){
             
-            for(int i=start;i<=ran;i++){
-                points[i] ^= points[(end+1)-(ran-i+1)];
-                points[(end+1)-(ran-i+1)] ^= points[i];
-                points[i] ^= points[(end+1)-(ran-i+1)];
+            for(int i=start;i<=rand;i++){
+                points[i] ^= points[(end+1)-(rand-i+1)];
+                points[(end+1)-(rand-i+1)] ^= points[i];
+                points[i] ^= points[(end+1)-(rand-i+1)];
             }
             
-            splite(points, start, end-(ran-start+1), ran);
+            splite(points, start, end-(rand-start+1), rand);
             
         }else{
             
-            for (int i=ran+1; i<=end; i++) {
-                points[i] ^= points[i-(ran-start+1)];
-                points[i-(ran-start+1)] ^= points[i];
-                points[i] ^= points[i-(ran-start+1)];
+            for (int i=rand+1; i<=end; i++) {
+                points[i] ^= points[i-(rand-start+1)];
+                points[i-(rand-start+1)] ^= points[i];
+                points[i] ^= points[i-(rand-start+1)];
             }
             
-            splite(points, start+(end-ran), end, ran);
+            splite(points, start+(end-rand), end, rand);
             
         }
     }else{
         //奇数个元素
-        if((ran-start+1)<(end-ran)){
+        if((rand-start+1)<(end-rand)){
             
-            for(int i=start;i<=ran;i++){
-                points[i] ^= points[(end+1)-(ran-i+1)];
-                points[(end+1)-(ran-i+1)] ^= points[i];
-                points[i] ^= points[(end+1)-(ran-i+1)];
+            for(int i=start;i<=rand;i++){
+                points[i] ^= points[(end+1)-(rand-i+1)];
+                points[(end+1)-(rand-i+1)] ^= points[i];
+                points[i] ^= points[(end+1)-(rand-i+1)];
             }
             
-            splite(points, start, end-(ran-start+1), ran);
+            splite(points, start, end-(rand-start+1), rand);
             
         }else{
             
-            for (int i=ran+1; i<=end; i++) {
-                points[i] ^= points[i-(ran-start+1)];
-                points[i-(ran-start+1)] ^= points[i];
-                points[i] ^= points[i-(ran-start+1)];
+            for (int i=rand+1; i<=end; i++) {
+                points[i] ^= points[i-(rand-start+1)];
+                points[i-(rand-start+1)] ^= points[i];
+                points[i] ^= points[i-(rand-start+1)];
             }
             
-            splite(points, start+(end-ran), end, ran);
+            splite(points, start+(end-rand), end, rand);
             
         }
     }
